45ae3bbc52177481493bd30d01282fb500b2c3da
Evgeniy Oshepkov <antarievg@yandex.ru>
2023-02-23 08:15:40 +0300

    fixed

--- script/index.js
+++ script/index.js
@@ -1,5 +1,7 @@
-import {el, setChildren} from '../node_modules/redom/dist/redom.es.js';
-import Inputmask from '../node_modules/inputmask/dist/inputmask.es6.js';
+// import {el, setChildren} from '../node_modules/redom/dist/redom.es.js';
+// import Inputmask from '../node_modules/inputmask/dist/inputmask.es6.js';
+import {el, setChildren} from 'redom';
+import Inputmask from 'inputmask';
 
 const wrapper = el('div', {className: 'wrapper'});
 const card = el('div', {className: 'card'});
@@ -18,7 +20,7 @@ const form = el('form', {id: 'form', className: 'form'});
 const holderField = el('div', {className:
   `form__input-wrap form__input-wrap_holder`});
 const holderInput = el('input', {className: 'input input__holder',
-  type: 'text'});
+  type: 'text', maxlength: 25});
 
 setChildren(holderField, el('label', {className: `form__label
   form__holder-label`, textContent: 'Card Holder'}), holderInput);
@@ -57,10 +59,19 @@ setChildren(card, [el('p', {className: 'secure',
 setChildren(wrapper, card);
 setChildren(document.body, wrapper);
 
-const im = new Inputmask('9999 9999 9999 9999');
-im.mask(numberInput);
+const im1 = new Inputmask('9999 9999 9999 9999');
+const im2 = new Inputmask('99/99');
+const im3 = new Inputmask('999');
+im1.mask(numberInput);
+im2.mask(dateInput);
+im3.mask(cvvInput);
 
 const showName = () => {
+  if (!holderInput.value.trim()) {
+    cardName.textContent = 'John Doe';
+    return;
+  }
+
   cardName.textContent = holderInput.value;
 };
 
@@ -72,17 +83,7 @@ const showDate = () => {
   cardDate.textContent = dateInput.value;
 };
 
-function debounce(callback, delay) {
-  let timer;
-  return function (...args) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      callback.apply(this, args);  
-    }, delay);
-  }
-};
-
-holderInput.addEventListener('input', debounce(showName, 300));
+holderInput.addEventListener('input', showName);
 numberInput.addEventListener('input', showNumber);
 dateInput.addEventListener('input', showDate);
 
